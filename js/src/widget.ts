55bddd417d58f5cd8c2b114998c291508767c377
fekad <adam@fekete.co.uk>
2020-05-07 19:32:34 +0200

    fixing indentations

--- js/src/widget.ts
+++ js/src/widget.ts
@@ -2,11 +2,11 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
-  DOMWidgetModel, DOMWidgetView, ISerializers, uuid
+    DOMWidgetModel, DOMWidgetView, ISerializers, uuid
 } from '@jupyter-widgets/base';
 
 import {
-  MODULE_NAME, MODULE_VERSION
+    MODULE_NAME, MODULE_VERSION
 } from './version';
 
 // Import the CSS
@@ -15,48 +15,46 @@ import '../css/widget.css'
 declare var Jmol: any;
 
 
-export
-class ExampleModel extends DOMWidgetModel {
-  defaults() {
-    return {...super.defaults(),
-      _model_name: ExampleModel.model_name,
-      _model_module: ExampleModel.model_module,
-      _model_module_version: ExampleModel.model_module_version,
-      _view_name: ExampleModel.view_name,
-      _view_module: ExampleModel.view_module,
-      _view_module_version: ExampleModel.view_module_version,
-      value : 'Hello World'
-    };
-  }
+export class ExampleModel extends DOMWidgetModel {
+    defaults() {
+        return {
+            ...super.defaults(),
+            _model_name: ExampleModel.model_name,
+            _model_module: ExampleModel.model_module,
+            _model_module_version: ExampleModel.model_module_version,
+            _view_name: ExampleModel.view_name,
+            _view_module: ExampleModel.view_module,
+            _view_module_version: ExampleModel.view_module_version,
+            value: 'Hello World'
+        };
+    }
 
-  static serializers: ISerializers = {
-      ...DOMWidgetModel.serializers,
-      // Add any extra serializers here
+    static serializers: ISerializers = {
+        ...DOMWidgetModel.serializers,
+        // Add any extra serializers here
     };
 
-  static model_name = 'ExampleModel';
-  static model_module = MODULE_NAME;
-  static model_module_version = MODULE_VERSION;
-  static view_name = 'ExampleView';   // Set to null if no view
-  static view_module = MODULE_NAME;   // Set to null if no view
-  static view_module_version = MODULE_VERSION;
+    static model_name = 'ExampleModel';
+    static model_module = MODULE_NAME;
+    static model_module_version = MODULE_VERSION;
+    static view_name = 'ExampleView';   // Set to null if no view
+    static view_module = MODULE_NAME;   // Set to null if no view
+    static view_module_version = MODULE_VERSION;
 }
 
 
-export
-class ExampleView extends DOMWidgetView {
-  render() {
-    this.el.classList.add('custom-widget');
+export class ExampleView extends DOMWidgetView {
+    render() {
+        this.el.classList.add('custom-widget');
 
-    this.value_changed();
-    this.model.on('change:value', this.value_changed, this);
-  }
+        this.value_changed();
+        this.model.on('change:value', this.value_changed, this);
+    }
 
-  value_changed() {
-    this.el.textContent = this.model.get('value');
-  }
+    value_changed() {
+        this.el.textContent = this.model.get('value');
+    }
 }
-
 
 
 // Custom Model. Custom widgets models must at least provide default values
@@ -75,88 +73,87 @@ class ExampleView extends DOMWidgetView {
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
 
-export
-class JsmolModel extends DOMWidgetModel {
-  defaults() {
-    return {...super.defaults(),
-      _model_name: JsmolModel.model_name,
-      _model_module: JsmolModel.model_module,
-      _model_module_version: JsmolModel.model_module_version,
-      _view_name: JsmolModel.view_name,
-      _view_module: JsmolModel.view_module,
-      _view_module_version: JsmolModel.view_module_version,
-      value : 'Hello World'
-    };
-  }
+export class JsmolModel extends DOMWidgetModel {
+    defaults() {
+        return {
+            ...super.defaults(),
+            _model_name: JsmolModel.model_name,
+            _model_module: JsmolModel.model_module,
+            _model_module_version: JsmolModel.model_module_version,
+            _view_name: JsmolModel.view_name,
+            _view_module: JsmolModel.view_module,
+            _view_module_version: JsmolModel.view_module_version,
+            value: 'Hello World'
+        };
+    }
 
-  static serializers: ISerializers = {
-      ...DOMWidgetModel.serializers,
-      // Add any extra serializers here
+    static serializers: ISerializers = {
+        ...DOMWidgetModel.serializers,
+        // Add any extra serializers here
     };
 
-  static model_name = 'JsmolModel';
-  static model_module = MODULE_NAME;
-  static model_module_version = MODULE_VERSION;
-  static view_name = 'JsmolView';   // Set to null if no view
-  static view_module = MODULE_NAME;   // Set to null if no view
-  static view_module_version = MODULE_VERSION;
+    static model_name = 'JsmolModel';
+    static model_module = MODULE_NAME;
+    static model_module_version = MODULE_VERSION;
+    static view_name = 'JsmolView';   // Set to null if no view
+    static view_module = MODULE_NAME;   // Set to null if no view
+    static view_module_version = MODULE_VERSION;
 }
 
 // Custom View. Renders the widget model.
 
-export
-class JsmolView extends DOMWidgetView {
-  private jsmol: any;
+export class JsmolView extends DOMWidgetView {
+    private jsmol: any;
 
-  // Defines how the widget gets rendered into the DOM
-  render() {
+    // Defines how the widget gets rendered into the DOM
+    render() {
 
-    // every view should have its own unique id
-    this.el.setAttribute("id", uuid());
+        // every view should have its own unique id
+        this.el.setAttribute("id", uuid());
 
-    // Create an Jmol applet
-    this.create_app();
+        // Create an Jmol applet
+        this.create_app();
 
-    // Observe changes in the value traitlet in Python, and define a custom callback.
-    // Python -> JavaScript update
-    // equivalent: this.listenTo(this.model, 'change:count', this._count_changed, this);
-    this.model.on('change:info', this.create_app, this);
-    this.model.on('change:_script', this.script, this);
-    this.model.on('change:_command', this.evaluate, this);
+        // Observe changes in the value traitlet in Python, and define a custom callback.
+        // Python -> JavaScript update
+        // equivalent: this.listenTo(this.model, 'change:count', this._count_changed, this);
+        this.model.on('change:info', this.create_app, this);
+        this.model.on('change:_script', this.script, this);
+        this.model.on('change:_command', this.evaluate, this);
 
-  }
-
-
-  create_app() {
-    // jsmol_id should be a valid js variable name because it will be used to generate the actual object
-    let jsmol_id = "jsmol_" + this.model.model_id;
-    let info = this.model.get('info');
-
-    Jmol.setDocument(false);
-    this.jsmol = Jmol.getApplet(jsmol_id, info);
-    console.log(this.jsmol);
-
-    // Finally the the content of the div should be generated
-    this.el.innerHTML = Jmol.getAppletHtml(this.jsmol);
-
-    // Jmol rely on this script being implicitly executed, but this is not
-    // the case when using innerHTML (compared to jquery .html()). So let's
-    // manually execute it
-    Jmol.coverApplet(this.jsmol);
-  }
+    }
 
 
-  script() {
-    let command = this.model.get('_script');
-    Jmol.script(this.jsmol, command);
-  }
+    create_app() {
+        // jsmol_id should be a valid js variable name because it will be used to generate the actual object
+        let jsmol_id = "jsmol_" + this.model.model_id;
+        let info = this.model.get('info');
+
+        Jmol.setDocument(false);
+        this.jsmol = Jmol.getApplet(jsmol_id, info);
+        console.log(this.jsmol);
+
+        // Finally the the content of the div should be generated
+        this.el.innerHTML = Jmol.getAppletHtml(this.jsmol);
+
+        // Jmol rely on this script being implicitly executed, but this is not
+        // the case when using innerHTML (compared to jquery .html()). So let's
+        // manually execute it
+        Jmol.coverApplet(this.jsmol);
+    }
 
 
-  evaluate() {
-    let command = this.model.get('_command');
-    console.log('evaluate: ' + command);
-    let value = Jmol.evaluateVar(this.jsmol, command);
-    console.log('value: ' + value);
-  }
+    script() {
+        let command = this.model.get('_script');
+        Jmol.script(this.jsmol, command);
+    }
+
+
+    evaluate() {
+        let command = this.model.get('_command');
+        console.log('evaluate: ' + command);
+        let value = Jmol.evaluateVar(this.jsmol, command);
+        console.log('value: ' + value);
+    }
 
 }
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,112 +3,112 @@ const version = require('./package.json').version;
 
 // Custom webpack rules
 const rules = [
-  { test: /\.ts$/, loader: 'ts-loader' },
-  { test: /\.js$/, loader: 'source-map-loader' },
-  { test: /\.css$/, use: ['style-loader', 'css-loader']}
+    {test: /\.ts$/, loader: 'ts-loader'},
+    {test: /\.js$/, loader: 'source-map-loader'},
+    {test: /\.css$/, use: ['style-loader', 'css-loader']}
 ];
 
 // Packages that shouldn't be bundled but loaded at runtime
 const externals = ['@jupyter-widgets/base'];
 
 const resolve = {
-  // Add '.ts' and '.tsx' as resolvable extensions.
-  extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
 };
 
 module.exports = [
-  /**
-   * Notebook extension
-   */
+    /**
+     * Notebook extension
+     */
 
-  /**
-   * Bundle for the notebook containing the custom widget views and models
-   * This bundle contains the implementation for the custom widget views and
-   * custom widget. It must be an amd module.
-   */
-  {
-    entry: './src/index.ts',
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, '..', 'jupyter_jsmol', 'nbextension', 'static'),
-      libraryTarget: 'amd'
+    /**
+     * Bundle for the notebook containing the custom widget views and models
+     * This bundle contains the implementation for the custom widget views and
+     * custom widget. It must be an amd module.
+     */
+    {
+        entry: './src/index.ts',
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, '..', 'jupyter_jsmol', 'nbextension', 'static'),
+            libraryTarget: 'amd'
+        },
+        module: {
+            rules: rules
+        },
+        devtool: 'source-map',
+        externals,
+        resolve,
     },
-    module: {
-      rules: rules
-    },
-    devtool: 'source-map',
-    externals,
-    resolve,
-  },
 
-  /**
-   * This bundle only contains the part of the JavaScript that is run on load of
-   * the notebook. This section generally only perform some configuration for
-   * requirejs, and provides the legacy "load_ipython_extension" function which
-   * is required for any notebook extension.
-   */
-  {
-    entry: './src/extension.ts',
-    output: {
-      filename: 'extension_new.js',
-      path: path.resolve(__dirname, '..', 'jupyter_jsmol', 'nbextension', 'static'),
-      libraryTarget: 'amd'
+    /**
+     * This bundle only contains the part of the JavaScript that is run on load of
+     * the notebook. This section generally only perform some configuration for
+     * requirejs, and provides the legacy "load_ipython_extension" function which
+     * is required for any notebook extension.
+     */
+    {
+        entry: './src/extension.ts',
+        output: {
+            filename: 'extension_new.js',
+            path: path.resolve(__dirname, '..', 'jupyter_jsmol', 'nbextension', 'static'),
+            libraryTarget: 'amd'
+        },
+        module: {
+            rules: [
+                {test: /\.ts$/, loader: 'ts-loader'},
+            ]
+        },
+        resolve,
     },
-    module: {
-      rules: [
-          { test: /\.ts$/, loader: 'ts-loader' },
-      ]
-    },
-    resolve,
-  },
 
-  /**
-   * Embeddable jupyter-jsmol bundle
-   *
-   * This bundle is almost identical to the notebook extension bundle. The only
-   * difference is in the configuration of the webpack public path for the
-   * static assets.
-   *
-   * The target bundle is always `dist/index.js`, which is the path required by
-   * the custom widget embedder.
-   */
-  {
-    entry: './src/index.ts',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'amd',
-        library: "jupyter-jsmol",
-        publicPath: 'https://unpkg.com/jupyter-jsmol@' + version + '/dist/'
+    /**
+     * Embeddable jupyter-jsmol bundle
+     *
+     * This bundle is almost identical to the notebook extension bundle. The only
+     * difference is in the configuration of the webpack public path for the
+     * static assets.
+     *
+     * The target bundle is always `dist/index.js`, which is the path required by
+     * the custom widget embedder.
+     */
+    {
+        entry: './src/index.ts',
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'amd',
+            library: "jupyter-jsmol",
+            publicPath: 'https://unpkg.com/jupyter-jsmol@' + version + '/dist/'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: rules
+        },
+        externals,
+        resolve,
     },
-    devtool: 'source-map',
-    module: {
-        rules: rules
-    },
-    externals,
-    resolve,
-  },
 
 
-  /**
-   * Documentation widget bundle
-   *
-   * This bundle is used to embed widgets in the package documentation.
-   */
-  {
-    entry: './src/index.ts',
-    output: {
-      filename: 'embed-bundle.js',
-      path: path.resolve(__dirname, '..', 'docs', 'static'),
-      library: "jupyter-jsmol",
-      libraryTarget: 'amd'
-    },
-    module: {
-      rules: rules
-    },
-    devtool: 'source-map',
-    externals,
-    resolve,
-  }
+    /**
+     * Documentation widget bundle
+     *
+     * This bundle is used to embed widgets in the package documentation.
+     */
+    {
+        entry: './src/index.ts',
+        output: {
+            filename: 'embed-bundle.js',
+            path: path.resolve(__dirname, '..', 'docs', 'static'),
+            library: "jupyter-jsmol",
+            libraryTarget: 'amd'
+        },
+        module: {
+            rules: rules
+        },
+        devtool: 'source-map',
+        externals,
+        resolve,
+    }
 
 ];
