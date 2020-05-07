36eb5543aece0a7a9d0ee0c4c2735206b3f36e8b
fekad <adam@fekete.co.uk>
2020-05-07 22:44:12 +0200

    excluding examples from pipy package

--- setup.py
+++ setup.py
@@ -29,30 +29,36 @@ cmdclass = create_cmdclass('jsdeps',
                            data_files_spec=data_files_spec)
 cmdclass['jsdeps'] = install_npm(js_path, build_cmd='build:all')
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup_args = dict(
-    name                 = 'jupyter_jsmol',
-    description          = 'JSmol viewer widget for Jupyter',
-    version              = '0.1.0',
-    author               = 'Adam Fekete',
-    author_email         = 'adam@fekete.co.uk',
-    url                  = 'https://github.com/fekad/jupyter-jsmol',
-    license              = 'BSD',
-    platforms            = "Linux, Mac OS X, Windows",
-    keywords             = ['Jupyter', 'Widgets', 'IPython'],
-    classifiers          = [
+    name='jupyter_jsmol',
+    description='JSmol viewer widget for Jupyter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.1.0',
+    author='Adam Fekete',
+    author_email='adam@fekete.co.uk',
+    url='https://github.com/fekad/jupyter-jsmol',
+    license='BSD',
+    platforms="Linux, Mac OS X, Windows",
+    keywords=['Jupyter', 'Widgets', 'IPython'],
+    classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Framework :: Jupyter',
     ],
-    packages             = find_packages(),
-    include_package_data = True,
-    cmdclass             = cmdclass,
-    install_requires     = [
+    packages=find_packages(),
+    cmdclass=cmdclass,
+    include_package_data=True,
+    python_requires='>=3.4',
+    install_requires=[
         'ipywidgets>=7.0.0',
     ],
-    extras_require       = {
+    extras_require={
         'test': [
             'pytest>=3.6',
         ],
