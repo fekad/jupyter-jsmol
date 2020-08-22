5fe9ec3b2c0285a43fcbd7ef26276e27723615df
Adam Fekete <adam.fekete@kcl.ac.uk>
2020-08-22 03:22:05 +0200

    updating npm packages and bumping up the version

--- jupyter_jsmol/_version.py
+++ jupyter_jsmol/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Adam Fekete.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 3)
+version_info = (0, 2, 4)
 __version__ = ".".join(map(str, version_info))
