cabaea3a5432f1b6054c2e97c29375f423ac9124
Adam Fekete <adam.fekete@kcl.ac.uk>
2021-01-27 17:45:30 +0100

    using args and kwargs fot the quickview method

--- jupyter_jsmol/pymatgen.py
+++ jupyter_jsmol/pymatgen.py
@@ -4,19 +4,24 @@
 try:
     from pymatgen import Structure
     from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+    from pymatgen.io.cif import CifWriter
+
     pymatgen_loaded = True
 except ImportError:
     raise RuntimeError('To use quick_view, you need to have pymatgen installed.')
 
 from jupyter_jsmol import JsmolView
 
-def quick_view(structure: Structure, conventional: bool = False, supercell: list = None) -> JsmolView:
+def quick_view(structure: Structure, *args, conventional: bool = False, supercell: list = None, **kwargs) -> JsmolView:
     """A function to visualize pymatgen Structure objects in jupyter notebook using jupyter_jsmol package.
 
     Args:
         structure: pymatgen Structure object.
         conventional: use conventional cell. Defaults to False.
-        transform: can be used to make supercells with pymatgen.Structure.make_supercell method.
+        supercell: can be used to make supercells with pymatgen.Structure.make_supercell method.
+        *args: Extra arguments for JSmol's load command. Eg. "{2 2 2}", "packed"
+        **kwargs: Kwargs passthru to CifWriter methods. E.g., This allows
+                the passing of parameters like symprec=0.01 for generation of symmetric cifs.
 
     Returns:
         A jupyter widget object.
@@ -29,4 +34,4 @@ def quick_view(structure: Structure, conventional: bool = False, supercell: list
     if supercell:
         s.make_supercell(supercell)
 
-    return JsmolView.from_str(s.to('cif'))
+    return JsmolView.from_str(str(CifWriter(s, **kwargs)), *args)
